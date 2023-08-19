4c65211aaa4bcda414fd479f588b0ded962fe5a9
dev-vickie <vmutethia84@gmail.com>
2023-08-19 12:22:53 +0300

    ADD: Firebase remote db

--- lib/features/todo/domain/entities/todo.dart
+++ lib/features/todo/domain/entities/todo.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 class Todo {
   final String id;
@@ -9,4 +11,24 @@ class Todo {
     required this.text,
     required this.description,
   });
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'text': text,
+      'description': description,
+    };
+  }
+
+  factory Todo.fromMap(Map<String, dynamic> map) {
+    return Todo(
+      id: map['id'] as String,
+      text: map['text'] as String,
+      description: map['description'] as String,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Todo.fromJson(String source) => Todo.fromMap(json.decode(source) as Map<String, dynamic>);
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:uniproject/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
